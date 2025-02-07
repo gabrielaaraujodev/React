4023fdb6d070511246c11ed88c3e123dded35cb0
gabrielaaraujodev <aaraujoslgabriel@gmail.com>
2025-02-07 16:59:04 -0300

    Carrying out slightly larger exercises with new concepts to continue the process of remembering basic structures to later move on.

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,50 @@
 import React from "react";
 
-// Mostre os dados da aplicação, como aprensetado no vídeo
-// Não utilize CSS externo, use o style para mudar as cores
-// Se a situação estiver ativa pinte de verde, inativa vermelho
-// Se o gasto for maior que 10000 mostre uma mensagem
-  
-  const luana = {
-    cliente: 'Luana',
-    idade: 27,
-    compras: [
-      { nome: 'Notebook', preco: 'R$ 2500' },
-      { nome: 'Geladeira', preco: 'R$ 3000' },
-      { nome: 'Smartphone', preco: 'R$ 1500' },
-    ],
-    ativa: true,
-  };
+// Organize os produtos como mostrado no vídeo
+// Mostre apenas produtos que forem mais caros que R$ 1500
+const produtos = [
+  {
+    id: 1,
+    nome: 'Smartphone',
+    preco: 'R$ 2000',
+    cores: ['#29d8d5', '#252a34', '#fc3766'],
+  },
+  {
+    id: 2,
+    nome: 'Notebook',
+    preco: 'R$ 3000',
+    cores: ['#ffd045', '#d4394b', '#f37c59'],
+  },
+  {
+    id: 3,
+    nome: 'Tablet',
+    preco: 'R$ 1500',
+    cores: ['#365069', '#47c1c8', '#f95786'],
+  },
+];
 
-  const mario = {
-    cliente: 'Mario',
-    idade: 31,
-    compras: [
-      { nome: 'Notebook', preco: 'R$ 2500' },
-      { nome: 'Geladeira', preco: 'R$ 3000' },
-      { nome: 'Smartphone', preco: 'R$ 1500' },
-      { nome: 'Guitarra', preco: 'R$ 3500' },
-    ],
-    ativa: false,
-};
-
-const somaPreco = (dados) => {
-  var valor = 0;
-  var valoresInt = dados.compras.map((valores) => Number(valores.preco.replace("R$ ", "")));
-  
-
-  valoresInt.forEach(
-    (preco) => {
-      valor += preco
-    }
-  )
-  return valor
-}
 
 const App = () => {
-  const dados = luana;
+
+  const dados = produtos.filter(
+    ({preco}) => Number(preco.replace("R$ ", "")) > 1500
+  )
 
   return (
     <div>
-      <p>Nome: {dados.cliente}</p>
-      <p>Idade: {dados.idade}</p>
-      <p>Situação: <span style={{color: dados.ativa ? 'green' : 'red'}}>{dados.ativa  == true ? "Ativo" : "Inativo"}</span></p>
-      <p>Total gasto: R$ {somaPreco(dados)}</p>
-      {somaPreco(dados) > 10000 && <p>Você está gastando muito</p>}
+      {dados.map(({id, nome, preco, cores}) => (
+          <div key={id}>
+            <h1>{nome}</h1>
+            <p>Preço: {preco}</p>
+            <ul>
+              {cores.map((cor) => (<li key={cor} style={{color: "black", backgroundColor: cor}}>{cor}</li>))}
+            </ul>
+          </div>
+        ))
+      }
     </div>
   )
+
 }
 
 export default App
